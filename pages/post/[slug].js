ce353fc2a93d78bb43b2f58718688f3a2be4a72f
CodeSevenic <sevenicbookings@gmail.com>
2022-09-12 14:51:05 +0200

    TS file update

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -33,3 +33,11 @@ const PostDetails = () => {
 };
 
 export default PostDetails;
+
+export async function getStaticProps({ params }) {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
