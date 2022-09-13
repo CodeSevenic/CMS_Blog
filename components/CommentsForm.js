c345d6e5d289bde83d2cae24f18a78ccf47410cb
CodeSevenic <sevenicbookings@gmail.com>
2022-09-13 13:40:23 +0200

    TS file update

--- components/CommentsForm.js
+++ components/CommentsForm.js
@@ -1,5 +1,7 @@
 ﻿import React, { useState, useEffect, useRef } from 'react';
 
+import { submitComment } from '../services';
+
 const CommentsForm = () => {
   const [error, setError] = useState(false);
   const [localStorage, setLocalStorage] = useState(null);
@@ -9,6 +11,11 @@ const CommentsForm = () => {
   const nameEl = useRef();
   const emailEl = useRef();
   const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name');
+    emailEl.current.value = window.localStorage.getItem('email');
+  }, []);
 
   const handleCommentSubmission = () => {
     setError(false);
@@ -37,12 +44,19 @@ const CommentsForm = () => {
       localStorage.removeItem('name', name);
       localStorage.removeItem('email', email);
     }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
   };
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
-        Comments Form
+        Leave a comment
       </h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea
