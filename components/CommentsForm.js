78c1599edaa7a566a9006c487ebd604418bba172
CodeSevenic <sevenicbookings@gmail.com>
2022-09-13 09:19:53 +0200

    TS file update

--- components/CommentsForm.js
+++ components/CommentsForm.js
@@ -1,6 +1,9 @@
-﻿import React from 'react';
+﻿import React, { useState, useEffect } from 'react';
 
 const CommentsForm = () => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   return (
     <div>
       <h1>Comments Form</h1>
