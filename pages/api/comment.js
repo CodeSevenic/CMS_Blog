2e8f308cd51e2a5e69c86478153b66044c81b639
CodeSevenic <sevenicbookings@gmail.com>
2022-09-13 12:55:06 +0200

    TS file update

---
+++ pages/api/comment.js
@@ -0,0 +1,4 @@
+/** *****************************************************
+ * Any file inside the folder pages/api is mapped to /api/* and
+ * will be treated as an API endpoint instead of a page
+ **********************************************************/
